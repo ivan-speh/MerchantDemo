dd1facb0c0d55512b84d98ab20946430ea041874
Brigita Perković <brigita.perkovic@gmail.com>
2022-03-18 15:34:19 +0100

    PrviKorak

---
+++ Domain.Entities/TransactionEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Domain.Entities
+{
+    public class TransactionEntity
+    {
+        public int Id { get; set; }
+        public double Amount { get; set; }
+        public string IsoCurrencyCode { get; set; }
+        public string TransactionId { get; set; }
+        public string AircashTransactionId { get; set; }
+        public DateTime DateTimeUTC { get; set; }
+    }
+}
